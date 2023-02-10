16d53f33963bc2069da071947ccf178dda7d4f58
duvan177 <duvannarvaez177@gmail.com>
2023-02-10 18:20:14 -0500

    head

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { domains: ['cdn.weatherapi.com'] },
+  headers: async () => {  
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
